d534880d142c3ab286484360996da443bd87bdd2
João Veloso <141861100+joaoVelosoT@users.noreply.github.com>
2024-04-14 17:42:38 -0300

    att

--- marvel/Marvel/Scripts/urlCharacters.js
+++ marvel/Marvel/Scripts/urlCharacters.js
@@ -6,7 +6,7 @@ async function getUrl() {
      const searchParams = url1.searchParams;
 
      const id = searchParams.get("id");
-     const url = `http://gateway.marvel.com/v1/public/comics/${id}?ts=1&apikey=8d4a83eebc0f87e8fd638fc68ab35c7a&hash=87e64d52b71494ef6898f69ea0650231`;
+     const url = `http://gateway.marvel.com/v1/public/characters/${id}?ts=1&apikey=8d4a83eebc0f87e8fd638fc68ab35c7a&hash=87e64d52b71494ef6898f69ea0650231`;
      const resp = await fetch(url);
      const dados = await resp.json();
      const data = dados.data.results;
@@ -14,15 +14,18 @@ async function getUrl() {
 
     var title = "";
     var writerName = "";
-
+    var desc = "";
     data.forEach(element => {
-         title = element.title;
-         const dataWriter = element.creators.items
+         title = element.name;
+         desc = element.description;
+         if(desc.length == 0){
+          desc = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque ea ut numquam odit molestias, voluptas odio harum blanditiis. Totam, magni officia! Aspernatur quas consectetur pariatur harum, delectus temporibus nobis quia."
+         }
+        console.log(element)
+         
          const capa = document.querySelector("#capa");
          capa.innerHTML = ` <img src="${element.thumbnail.path.concat(`.${element.thumbnail.extension}`)}" alt="1">`
-         dataWriter.forEach(element => {
-            writerName = element.name
-         })
+       
     });
 
 
@@ -30,8 +33,7 @@ async function getUrl() {
     const container = document.querySelector("#nome");
     // const div = document.createElement("div");
     container.innerHTML = ` <h1>${title}</h1>
-    <h3>Writer: ${writerName}</h3>
-    <h5>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae hic error assumenda similique aut totam iure reiciendis ullam nobis quam, veritatis ea, vel, blanditiis aliquid perferendis voluptatibus? Omnis, dignissimos eaque?</h5>
+    <h5>${desc}</h5>
   `
 }
 
