67e8ff40fb1907f850135b28a24fd8049cc3c7b1
João Veloso <141861100+joaoVelosoT@users.noreply.github.com>
2024-04-22 13:46:15 -0300

    att

--- marvel/Marvel/Scripts/characters.js
+++ marvel/Marvel/Scripts/characters.js
@@ -1,13 +1,15 @@
 async function getCharacters(){
     const response = await fetch("http://gateway.marvel.com/v1/public/characters?ts=1&apikey=8d4a83eebc0f87e8fd638fc68ab35c7a&hash=87e64d52b71494ef6898f69ea0650231");
     const personagens = await response.json();
+  
     const data = personagens.data.results;
+    console.log(data)
     const pai = document.getElementById("dad");
     var writer = ""
     data.forEach(element => {
         const dataCreators = element.urls;
         dataCreators.forEach(element => {
-            
+            console.log(element)
             writer = element.url;
          
         })
