1c79d01430c0bf94c5fbdf473990caa7ba8ad8ec
João Veloso <141861100+joaoVelosoT@users.noreply.github.com>
2024-04-23 15:40:19 -0300

    att

--- marvel/Marvel/Scripts/comics.js
+++ marvel/Marvel/Scripts/comics.js
@@ -4,7 +4,6 @@ async function getComics(){
     const response = await fetch("http://gateway.marvel.com/v1/public/comics?ts=1&apikey=8d4a83eebc0f87e8fd638fc68ab35c7a&hash=87e64d52b71494ef6898f69ea0650231");
     const personagens = await response.json();
 
-
     const dataArray = personagens.data.results;
     const pai = document.getElementById("comicDad");
     var writer = ""
